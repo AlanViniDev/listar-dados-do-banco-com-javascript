c9c5da45e59f559e0592acf108004b8d75139b7c
Alan Vinicius <alan123vinicius@gmail.com>
2020-11-21 01:46:00 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- Chama a biblioteca jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script type = "text/javascript">
+jQuery(document).ready(function(){
+var param = 10;
+    jQuery.ajax({
+        url: 'teste2.php',
+        ascyn: false,
+        data:{
+            'param': param
+        },
+        type: 'POST',
+        dataType: 'html',
+        success: function (data){
+            var dados = [];
+            dados.push(JSON.parse(data));
+            dados.forEach((elem)=>{
+                console.log(elem.idprod);
+                console.log(elem.nome);
+                console.log(elem.cor);
+                console.log(elem.preco);
+                document.getElementById("resultado").innerHTML =  elem.idprod + ' ' + elem.nome + ' ' + elem.cor + ' ' + elem.preco;
+            });
+        }
+    });
+});
+</script>
+<div id = "resultado"></div>
